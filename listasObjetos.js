b3129d4ff02c19ac5cf17d2184caa59f3f8c6e1b
richi0719 <ricardo.daniel0719@gmail.com>
2022-06-29 23:34:35 -0500

    Ejercicio de crear una lista con diferentes datos personales

---
+++ listasObjetos.js
@@ -0,0 +1,12 @@
+const datosPersonales = {
+    nombre: "Daniel",
+    edad: 31,
+    desarrollador: true,
+    libroFavorito: {
+        titulo: "Caballo de Troya",
+        autor: "J.J. Benitez"
+    }
+}
+
+console.log(datosPersonales)
+
